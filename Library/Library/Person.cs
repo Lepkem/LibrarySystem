d5669eb6638d1befa58bfd51d0d9c84b37fc2ae7
Lepkem <Lepkem@users.noreply.github.com>
2020-04-11 12:20:45 +0200

    added properties gender, passw, usern, status to Person class

--- Library/Library/Person.cs
+++ Library/Library/Person.cs
@@ -4,29 +4,74 @@ namespace Library
 {
     public class Person
     {
-        public Person(string initials, string surname, string streetName="unknown", 
+        // var author = new Person() 
+        // var aut = new Person("a","sss") 
+        
+
+        // enum - author - librarian - customer 
+        // property of th above type within the person class 
+        // not modifiable - set up once during creation 
+        // not visible outside of the class 
+
+        public Person(string firstname, string surname, PersonKind status, string username, string password)
+        {
+            this.ID = "PID" + Guid.NewGuid();
+            this.Status = status;
+            this.Firstname = firstname;
+            this.Surname = surname;
+            this.Username = username;
+            this.Password = password;
+            this.Gender = Gend.Unknown;
+            this.StreetName = "unknown";
+            this.ZipCode = "unknown";
+            this.City = "unknown";
+            this.EmailAddress = "unknown";
+            this.TelephoneNumber = "unknown";
+
+        }
+        public Person(string firstname, string surname, Gend gender =Gend.Unknown, string streetName="unknown", 
             string zipcode= "unknown", string city= "unknown", string emailAddress = "unknown",
-            string telephoneNumber= "unknown"
+            string telephoneNumber= "unknown", PersonKind status = PersonKind.Customer
         )
         {
-            this.Initials = initials;
+            this.ID = "PID" + Guid.NewGuid();
+            this.Firstname = firstname;
             this.Surname = surname;
+            this.Status = status;
+            this.Gender = gender;
             this.StreetName = streetName;
             this.ZipCode = zipcode;
             this.City = city;
             this.EmailAddress = emailAddress;
             this.TelephoneNumber = telephoneNumber;
-            this.ID = "PID" + Guid.NewGuid();
+            
 
         }
         protected string ID { get;}
 
-        protected string Initials { get; set; }
+        protected string Firstname { get; set; }
         protected string Surname { get; set; }
+        protected Gend Gender { get; set; }
+
+        public enum Gend
+        {
+            Male,
+            Female,
+            Unknown
+        }
         protected string StreetName { get; set; }
         protected string ZipCode { get; set; } 
         protected string City { get; set; }
         protected string EmailAddress { get; set; }
         protected string TelephoneNumber { get; set; }
+        protected PersonKind Status { get; }
+        public enum PersonKind
+        {
+            Librarian,
+            Customer,
+            Author
+        }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
