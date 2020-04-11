046accc5e84112b23d34fe1f776993b75ff151d3
Lepkem <Lepkem@users.noreply.github.com>
2020-04-11 10:54:54 +0200

    added Book class

---
+++ Library/Library/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace Library
+{
+    public class Book
+    {
+        public Book(Person author, string country = "unknown", string imageLink = "unknown",
+            string language = "unknown", string link = "unknown", uint pages = 0,
+            string title = "unknown", DateTime year = DateTime.Now)
+        {
+            this.Author = author;
+            this.Country = country;
+            this.ImageLink = imageLink;
+            this.Language = language;
+            this.Link = link;
+            this.Pages = pages;
+            this.Title = title;
+            this.Year = year;
+            this.ISBN = "ISBN" + Guid.NewGuid();
+        }
+
+        public Person Author { get; }
+        public string Country { get;}      //from JSON
+        public string ImageLink { get; protected set; }    //from JSON
+        public string Language { get;}     //from JSON
+        public string Link { get; protected set;}         //from JSON
+        public uint Pages { get;}          //from JSON
+        public string Title { get;}        //from JSON
+        public DateTime Year { get;}       //from JSON
+        public string ISBN { get;}
+        public uint BookQuantity;   //this one should be created by counting the BookItems of this book
+
+
+    }
+}
--- Library/Library/Person.cs
+++ Library/Library/Person.cs
@@ -1,22 +1,25 @@
-﻿namespace Library
+﻿using System;
+
+namespace Library
 {
     public class Person
     {
-        public Person(string initals, string surname, string streetName="unknown", 
+        public Person(string initials, string surname, string streetName="unknown", 
             string zipcode= "unknown", string city= "unknown", string emailAddress = "unknown",
             string telephoneNumber= "unknown"
         )
         {
-            this.Initials = initals;
+            this.Initials = initials;
             this.Surname = surname;
             this.StreetName = streetName;
             this.ZipCode = zipcode;
             this.City = city;
             this.EmailAddress = emailAddress;
             this.TelephoneNumber = telephoneNumber;
+            this.ID = "PID" + Guid.NewGuid();
 
         }
-        protected string ID;
+        protected string ID { get;}
 
         protected string Initials { get; set; }
         protected string Surname { get; set; }
