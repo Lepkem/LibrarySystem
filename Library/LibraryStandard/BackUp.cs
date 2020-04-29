05822c2350292e34731b8103a56581ca567c257e
Lepkem <Lepkem@users.noreply.github.com>
2020-04-29 08:54:42 +0200

    + Backup class

---
+++ Library/LibraryStandard/BackUp.cs
@@ -0,0 +1,20 @@
+﻿namespace LibraryStandard
+{
+    using System;
+
+    public class BackUp : IBackUp
+    {
+        public void Create()
+        {
+            // create json file in folder backups
+            // serialize Books into json file
+            // make timestamp
+            throw new System.NotImplementedException();
+        }
+
+        public void RestoreProgram(string jsonPath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
